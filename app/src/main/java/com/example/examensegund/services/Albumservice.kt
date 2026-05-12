fa0d03857109f021a9f0d21a61c2d917d1db30ba
Sebastien <vdjuniorr@gmail.com>
2026-05-12 16:53:53 -0600

    Segundo commit

---
+++ app/src/main/java/com/example/examensegund/services/Albumservice.kt
@@ -0,0 +1,13 @@
+package com.example.examensegund.services
+
+import com.example.examensegund.models.Album
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AlbumService {
+    @GET("api/albums")
+    suspend fun getAllAlbums(): List<Album>
+
+    @GET("api/albums/{id}")
+    suspend fun getAlbumById(@Path("id") id: Int): Album
+}
